42391af4e47b9042950313ee4c1b0d733701ce1f
PolarisDane <2488721971@qq.com>
2023-11-23 01:15:26 +0800

    feat(riscv): added Trying to implement a 1-stage pipeline first for better understanding and future work of vector architecture

---
+++ riscv/src/alu.v
@@ -0,0 +1,13 @@
+module alu (
+    input wire                 clk_in;
+    input wire                 rst_in;
+    input wire                 rdy_in;
+
+    input wire                 opType;
+
+    output reg                 aluResult;
+);
+
+always @(posedge clk_in) begin
+
+end
